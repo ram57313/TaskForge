9c293b7968053a44e530767c73c53f4ae0fa3edc
ram57313 <kudukaramcharan@gmail.com>
2026-09-03 17:15:45 +0530

    deployment work

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,8 +18,8 @@ const createSendToken=(user,statusCode,res)=>{
     const cookieOptions={
         expires:new Date(Date.now()+(process.env.JWT_COOKIE_EXPIRES_IN)*24*60*60*1000),
         httpOnly:true,
-        secure:false,//sent only when we use https
-        sameSite:"lax"
+        secure:process.env.NODE_ENV==="production",//sent only when we use https
+        sameSite:process.env.NODE_ENV==="production"?"none":"lax"
     }
     
     // if(process.env.NODE_ENV==='production')cookieOptions.secure=true;
